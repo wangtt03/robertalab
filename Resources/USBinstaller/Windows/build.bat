f48e45cfad83e5d406705b1da42d18cd09e3fd13
Jost, Beate <beate.jost@iais.fraunhofer.de>
2016-04-27 12:46:54 +0200

    #121 fix USB-EV3 programm

--- Resources/USBinstaller/Windows/build.bat
+++ Resources/USBinstaller/Windows/build.bat
@@ -1,3 +1,3 @@
-candle lib.wxs java.wxs setup.wxs
-light -out OpenRobertaEV3.msi -ext WixUIExtension -cultures:de-DE lib.wixobj java.wixobj setup.wixobj -b ./lib -b ./java
+candle lib.wxs firmware.wxs java.wxs setup.wxs
+light -out OpenRobertaEV3.msi -ext WixUIExtension -cultures:de-DE lib.wixobj firmware.wixobj java.wixobj setup.wixobj -b ./lib -b ./firmware-1.2 -b ./java
 @pause
