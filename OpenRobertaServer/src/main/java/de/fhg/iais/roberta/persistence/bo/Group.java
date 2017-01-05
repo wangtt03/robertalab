025eddba6147e5f38d542f9ac3161734ed7f7d01
Evgeniya Ovchinnikova <elhed@yandex.ru>
2017-01-05 14:41:45 +0100

    preliminary implementation of group persistences

--- OpenRobertaServer/src/main/java/de/fhg/iais/roberta/persistence/bo/Group.java
+++ OpenRobertaServer/src/main/java/de/fhg/iais/roberta/persistence/bo/Group.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import de.fhg.iais.roberta.util.dbc.Assert;
+
 @Entity
 @Table(name = "GROUP")
 public class Group implements WithSurrogateId {
@@ -20,6 +22,13 @@ public class Group implements WithSurrogateId {
   
     protected Group() {
         // Hibernate
+    }
+
+    public Group(String name, int ownerId) {
+        Assert.notNull(name);
+        Assert.notNull(ownerId);
+        this.name = name;
+        this.ownerId = ownerId;
     }
 
     /**
