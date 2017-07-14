0ad52f66c59f65f694747f1c225fe9676e5a4c93
Evgeniya Ovchinnikova <elhed@yandex.ru>
2017-07-14 14:40:49 +0200

    #483 moved assignment of variables inside setup so the variables initialized with functions wouldn't break -- only bnr for now

--- RobotArdu/resources/libraries/RobertaFunctions/RobertaFunctions.cpp
+++ RobotArdu/resources/libraries/RobertaFunctions/RobertaFunctions.cpp
@@ -4,44 +4,256 @@
   Created by Evgeniya Ovchinnikova, August 30, 2016.
   Released into the public domain.
 */
-
-#ifndef RobertaFunctions_h
-#define RobertaFunctions_h
-
 #include <stdio.h>
 #include <stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include "Arduino.h"
+#include "SPI.h"
+#include "math.h"
+#include "RobertaFunctions.h"
 
 
-class RobertaFunctions
-{
-  public:
-		int randomIntegerInRange(int val1, int val2);
-		float randomFloat();
-		double clamp(double val, double min, double max);
-		bool isPrime(double number);
-		bool isWhole(double val);
-	    void createArray(double *arr, int len, double firstItem, ...);
-	    void createArray(double *arr, int len, int firstItem, ...);
-	    void createArray(bool   *arr, int len, ...);
-	    void createArray(char  **arr, int len, ...);
-	    void createArray(int    *arr, int len, ...);
-		int arrFindFirst(int len, double arr[], double item);
-		int arrFindFirst(int len,   bool arr[],   bool item);
-		int arrFindFirst(int len, char **arr,   String item);
-		int arrFindLast( int len, double arr[], double item);
-		int arrFindLast( int len,   bool arr[],   bool item);
-		int arrFindLast( int len, char **arr, String item);
-		double arrSum(int len, double arr[]);
-		double arrMin(int len, double arr[]);
-		double arrMax(int len, double arr[]);
-		double arrMean(int len, double arr[]);
-		void arrInsertionSort(int len,  double *arr);
-		double arrMedian(int len, double arr[]);
-		double arrStandardDeviatioin(int len, double arr[]);
-		double arrRand(int len, double arr[]);
-		double arrMode(int len, double arr[]);
-};
-#endif
+int RobertaFunctions::randomIntegerInRange(int val1, int val2){
+	int min = fmin(val1, val2);
+	int max = fmax(val1, val2);
+	return min + (rand()%(min - max));
+}
+
+float RobertaFunctions::randomFloat(){
+	return (float)rand()/(float)RAND_MAX;
+}
+
+double RobertaFunctions::clamp(double val, double min, double max){
+	return fmin(fmax(val, min), max);
+}
+
+bool RobertaFunctions::isPrime(double number) {
+    if ((fmod(number, 2) == 0) || (number == 1)) return false;
+    //if not, then just check the odds
+    for(int i = 3; i * i <= number; i += 2) {
+        if(fmod(number, i) == 0)
+            return false;
+    }
+    return true;
+}
+
+bool RobertaFunctions::isWhole(double val){
+  int intPart = val;
+  return ((val - intPart) == 0);
+}
+
+void RobertaFunctions::createArray(double *arr, int len, double firstItem, ...){
+   va_list arguments;
+	 arr[0] = firstItem;
+   va_start ( arguments, firstItem );
+   for(int i = 1; i < len; i++){
+    	arr[i] =  va_arg ( arguments, double );
+    }
+    va_end ( arguments );
+}
+
+void RobertaFunctions::createArray(double *arr, int len, int firstItem, ...){
+   va_list arguments;
+	 arr[0] = firstItem;
+   va_start ( arguments, firstItem );
+   for(int i = 1; i < len; i++){
+    	arr[i] =  va_arg ( arguments, int );
+    }
+    va_end ( arguments );
+}
+
+void RobertaFunctions::createArray(bool *arr, int len, ...){
+   va_list arguments;
+   va_start ( arguments, len );
+   for(int i = 0; i < len; i++){
+     arr[i] = va_arg ( arguments, bool );
+    }
+    va_end ( arguments );
+}
+
+void RobertaFunctions::createArray(char **arr, int len, ...){
+   va_list arguments;
+   va_start ( arguments, len );
+   for(int i = 0; i < len; i++){
+     arr[i] = va_arg ( arguments, char* );
+    }
+    va_end ( arguments );
+}
+
+void RobertaFunctions::createArray(int *arr, int len, ...){
+   va_list arguments;
+   va_start ( arguments, len );
+   for(int i = 0; i < len; i++){
+     arr[i] = va_arg ( arguments, int );
+    }
+    va_end ( arguments );
+}
+
+int RobertaFunctions::arrFindFirst(int len, double arr[], double item) {
+  int i = 0;
+  if (arr[0] == item){
+    return i;
+  } else {
+    do {
+      i++;
+    } while((arr[i] != item) && (i != len));
+    return i;
+  }
+}
+
+int RobertaFunctions::arrFindFirst(int len, bool arr[], bool item) {
+  int i = 0;
+  if (arr[0] == item){
+    return i;
+  } else {
+    do {
+      i++;
+    } while((arr[i] != item) && (i != len));
+    return i;
+  }
+}
+
+int RobertaFunctions::arrFindFirst(int len, char **arr, String item) {
+  int i = 0;
+  if (strcmp(arr[0], item.c_str()) == 0){
+    return i;
+  } else {
+    do {
+      i++;
+    } while((strcmp(arr[i], item.c_str()) != 0) && (i != len));
+    return i;
+  }
+}
+
+
+int RobertaFunctions::arrFindLast(int len, double arr[], double item) {
+  int i = 0;
+  if (arr[len - 1] == item){
+    return len - 1 - i;
+  } else {
+    do {
+      i++;
+    } while((arr[len - 1 - i] != item)&&(i != 0));
+      return len - 1 - i;
+  }
+}
+
+int RobertaFunctions::arrFindLast(int len, bool arr[], bool item) {
+  int i = 0;
+  if (arr[len - 1] == item){
+    return len - 1 - i;
+  } else {
+    do {
+      i++;
+    } while((arr[len - 1 - i] != item)&&(i != 0));
+      return len - 1 - i;
+  }
+}
+int RobertaFunctions::arrFindLast(int len, char ** arr, String item) {
+  int i = 0;
+  if (strcmp(arr[len - 1], item.c_str()) == 0){
+    return len - 1 - i;
+  } else {
+    do {
+      i++;
+		} while( (strcmp( arr[len - 1 - i], item.c_str() ) != 0) && (i != 0) );
+      return len - 1 - i;
+  }
+}
+
+
+
+
+double RobertaFunctions::arrSum(int len, double arr[]) {
+  float sum = 0;
+  for(int i = 0; i < len; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+double RobertaFunctions::arrMin(int len, double arr[]) {
+  double min = arr[0];
+  for(int i = 1; i < len; i++) {
+    if (arr[i] < min){
+      min = arr[i];
+    }
+  }
+  return min;
+}
+double RobertaFunctions::arrMax(int len, double arr[]) {
+  double max = arr[0];
+  for(int i = 1; i < len; i++) {
+    if (arr[i] > max){
+      max = arr[i];
+    }
+  }
+  return max;
+}
+double RobertaFunctions::arrMean(int len, double arr[]) {
+  double sum = 0;
+  for(int i = 0; i < len; i++) {
+    sum += arr[i];
+  }
+  return sum/len;
+}
+void RobertaFunctions::arrInsertionSort(int len,  double *arr) {
+  for (int i=1; i < len; i++) {
+    int index = arr[i];
+    int j = i;
+    while (j > 0 && arr[j-1] > index) {
+      arr[j] = arr[j-1];
+      j--;
+    }
+    arr[j] = index;
+  }
+}
+double RobertaFunctions::arrMedian(int len, double arr[]) {
+  if (len == 0) {
+    return 0;
+  }
+  arrInsertionSort(len, arr);
+  double median;
+  if (len % 2 == 0) {
+    median = (arr[len / 2] + arr[len / 2 - 1]) / 2;
+  } else {
+    median = arr[len / 2];
+  }
+  return median;
+}
+
+double RobertaFunctions::arrStandardDeviatioin(int len, double arr[]) {
+  if (len == 0) {
+    return 0;
+  }
+  double variance = 0;
+  double mean = arrMean(len, arr);
+  for (int i = 0; i < len; i++) {
+    variance += pow(arr[i] - mean, 2);
+  }
+  variance /= len;
+  return sqrt(variance);
+}
+
+double RobertaFunctions::arrRand(int len, double arr[]) {
+  int arrayInd = len * (randomFloat()*100)/100;
+  return arr[arrayInd - 1];
+}
+
+double RobertaFunctions::arrMode(int len, double arr[]){
+  arrInsertionSort(len, arr);
+  double element = arr[0];
+  double max_seen = element;
+  int count = 1;
+  int mode_count = 1;
+  for (int i = 1; i < len; i++){
+    if (arr[i] == element){
+      count++;
+      if (count > mode_count) {
+        mode_count = count;
+        max_seen = element;
+      }
+    } else {
+      element = arr[i];
+      count = 1;
+    }
+  }
+  return max_seen;
+}
